ac552cbbae148ed1260ad9f62473b42e90d824cd
philippecomputer <108224168+philippe015@users.noreply.github.com>
2025-08-06 17:34:35 +0200

    Update fleet_carrier_assistant.py

--- assistants/fleet_carrier_assistant.py
+++ assistants/fleet_carrier_assistant.py
@@ -1,1 +1,22 @@
+from time import sleep
+from EDlogger import logger
 
+class FleetCarrierAssistant:
+    def __init__(self, ed_ap, callback):
+        self.ed_ap = ed_ap
+        self.callback = callback
+        self.active = False
+
+    def start(self):
+        logger.debug("Fleet Carrier Assist started")
+        self.active = True
+        self.callback('log+vce', "Fleet Carrier assist activated.")
+        self.callback('log', "Simulated Fleet Carrier jump initiated.")
+    
+    def stop(self):
+        logger.debug("Fleet Carrier Assist stopped")
+        self.active = False
+        self.callback('log+vce', "Fleet Carrier assist deactivated.")
+
+    def is_running(self):
+        return self.active
